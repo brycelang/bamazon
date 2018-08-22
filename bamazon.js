25fbdc77ac970b00de3873fe8e7b4c6d291a2317
bryce <brycelang@me.com>
2018-08-22 14:19:52 -0700

    mysql connection

--- bamazon.js
+++ bamazon.js
@@ -1,4 +1,5 @@
 const signale = require('signale');
+const mysql = require('mysql');
 
 // Overrides any existing `package.json` config
 signale.config({
@@ -8,3 +9,18 @@ signale.config({
 }); 
 
 signale.success('packages loaded');
+
+//creates a connection to the database
+var databse = mysql.createConnection({
+  host:"localhost",
+  port:'3306',
+  user:"root",
+  database:"bamazon"
+});
+
+//init the connection and returns any errors
+databse.connect(function(error){
+  if(error) throw error;
+signale.success('mysql loaded');
+});
+
